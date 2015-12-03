e4034439cfbb3e0e34f3469e5eedbfbec5297381
daywrite@yeah.net <daywrite@yeah.net>
2015-12-03 16:16:48 +0800

    界面

--- Lwb.Crawler.Center/Program.cs
+++ Lwb.Crawler.Center/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Lwb.Crawler.Center.Scheme;
 using Lwb.Crawler.Center.Server;
 using Lwb.Crawler.Service.Crawl;
 
