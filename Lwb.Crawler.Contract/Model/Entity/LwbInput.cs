5d8c8388ff29af03b1536e4abbb38528739c1c29
daywrite@yeah.net <daywrite@yeah.net>
2015-12-09 20:07:51 +0800

    Wcf-LwbInput实体类

--- Lwb.Crawler.Contract/Model/Entity/LwbInput.cs
+++ Lwb.Crawler.Contract/Model/Entity/LwbInput.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lwb.Crawler.Contract.Model
 {
+    [DataContract]
     public class LwbInput
     {
+
+        [DataMember]
         public int Type { get; set; }
+        [DataMember]
         public object Data { get; set; }
     }
-
+    [DataContract]
     public class Input获取生产线任务列表
     {
+        [DataMember]
         public int TaskMax { get; set; }
+        [DataMember]
         public List<string> RuningTaskHost { get; set; }
     }
 }
